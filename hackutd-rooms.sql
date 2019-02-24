c4193dee142c20b410133295d5f048ac107c9815
Ishpreet Singh Bhasin <ishpreet2000@gmail.com>
2019-02-24 12:25:14 -0600

    Sample data

--- hackutd-rooms.sql
+++ hackutd-rooms.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost
--- Generation Time: Feb 23, 2019 at 10:28 PM
+-- Generation Time: Feb 24, 2019 at 07:19 PM
 -- Server version: 10.1.38-MariaDB
 -- PHP Version: 7.3.2
 
@@ -25,13 +25,88 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
+-- Table structure for table `building`
+--
+
+CREATE TABLE `building` (
+  `id` int(11) NOT NULL,
+  `name` varchar(128) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `building`
+--
+
+INSERT INTO `building` (`id`, `name`) VALUES
+(1, 'ECS West'),
+(2, 'RH South'),
+(3, 'RH Northwest');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `checkin`
+--
+
+CREATE TABLE `checkin` (
+  `id` int(11) NOT NULL,
+  `user_id` char(21) NOT NULL,
+  `room_id` int(11) NOT NULL,
+  `event_id` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `event`
+--
+
+CREATE TABLE `event` (
+  `id` int(11) NOT NULL,
+  `title` varchar(128) NOT NULL,
+  `description` varchar(1000) NOT NULL DEFAULT '',
+  `study` tinyint(1) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `room`
+--
+
+CREATE TABLE `room` (
+  `id` int(11) NOT NULL,
+  `building_id` int(11) NOT NULL,
+  `number_floor` int(11) NOT NULL DEFAULT '1',
+  `number_extension` char(3) NOT NULL DEFAULT '000',
+  `name` varchar(128) NOT NULL DEFAULT 'Room',
+  `private` tinyint(1) NOT NULL DEFAULT '0',
+  `occupied` tinyint(1) NOT NULL DEFAULT '0'
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `room`
+--
+
+INSERT INTO `room` (`id`, `building_id`, `number_floor`, `number_extension`, `name`, `private`, `occupied`) VALUES
+(2, 1, 2, '305', 'Ideation Room', 0, 0),
+(3, 1, 2, '306', 'Corner Room', 0, 0),
+(4, 3, 1, '301', 'Study Room', 0, 0),
+(5, 3, 1, '401', 'Study Room', 0, 0),
+(6, 3, 2, '301', 'Study Room', 0, 0),
+(7, 3, 2, '401', 'TV Room', 0, 0),
+(8, 2, 3, '401', 'Study Room', 0, 0);
+
+-- --------------------------------------------------------
+
+--
 -- Table structure for table `user`
 --
 
 CREATE TABLE `user` (
   `user_id` char(21) NOT NULL,
   `name` varchar(128) NOT NULL,
-  `email` varchar(254) NOT NULL
+  `display_name` varchar(254) NOT NULL
 ) ENGINE=MyISAM DEFAULT CHARSET=latin1;
 
 --
@@ -39,10 +114,62 @@ CREATE TABLE `user` (
 --
 
 --
+-- Indexes for table `building`
+--
+ALTER TABLE `building`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `checkin`
+--
+ALTER TABLE `checkin`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `event`
+--
+ALTER TABLE `event`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `room`
+--
+ALTER TABLE `room`
+  ADD PRIMARY KEY (`id`);
+
+--
 -- Indexes for table `user`
 --
 ALTER TABLE `user`
   ADD PRIMARY KEY (`user_id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `building`
+--
+ALTER TABLE `building`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
+-- AUTO_INCREMENT for table `checkin`
+--
+ALTER TABLE `checkin`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=243;
+
+--
+-- AUTO_INCREMENT for table `event`
+--
+ALTER TABLE `event`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=53;
+
+--
+-- AUTO_INCREMENT for table `room`
+--
+ALTER TABLE `room`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
